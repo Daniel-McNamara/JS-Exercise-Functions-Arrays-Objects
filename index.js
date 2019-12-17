1530e0f201f17caa0693ef1dcd9ca476867c9877
daniel mcnamara <danmac8407@gmail.com>
2019-12-17 15:50:26 -0600

    pushing to finish

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ function getName(name){
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+  
 }
 
 
